186bb868376f54a7d585527e72a7858836fccd74
tianxin <tianxin@localhost>
2025-02-19 15:46:28 -0600

    studio 4

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,21 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		StdDraw.clear();
+		StdDraw.setPenColor(Color.GREEN);
+		StdDraw.filledRectangle(1, 1, 3, 3);
+		StdDraw.setPenColor(Color.BLUE);
+		StdDraw.filledRectangle(0, 0, 3, 0.1);
+		StdDraw.setPenColor(Color.BLUE);
+		StdDraw.filledRectangle(0, 0.25, 3, 0.05);
+		StdDraw.setPenColor(Color.BLUE);
+		StdDraw.filledRectangle(0, 1, 3, 0.1);
+		StdDraw.setPenColor(Color.WHITE);
+		StdDraw.filledRectangle(0.5, 0, 0.1, 3);
+		StdDraw.setPenColor(Color.RED);
+		StdDraw.filledCircle(0.5, 0.5, 0.2);
+
+		
 		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -8,6 +8,8 @@ import javax.swing.JFileChooser;
 import javax.swing.SwingUtilities;
 
 import edu.princeton.cs.introcs.StdDraw;
+import java.awt.Color;
+
 
 /**
  * @author Dennis Cosgrove (http://www.cse.wustl.edu/~cosgroved/)
@@ -19,6 +21,47 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
-		
+
+		StdDraw.clear();
+
+		String shape = in.next();
+		int red = in.nextInt();
+		int green = in.nextInt();
+		int blue = in.nextInt();
+		boolean filled = in.nextBoolean();
+		double x1 = in.nextDouble();
+		double y1 = in.nextDouble();
+		double width = in.nextDouble();
+		double height = in.nextDouble();
+
+
+
+		Color color = new Color(red,green,blue);
+		StdDraw.setPenColor(color);
+
+		while(true) {
+			if(shape.equals("rectangle")) {
+				if(filled == true) {
+					StdDraw.filledRectangle(x1, y1, width, height);
+				}
+				if(filled == false) {
+					StdDraw.rectangle(x1, y1, width, height);}
+			}
+			if(shape.equals("ellipse")) {
+				if(filled == true) {
+					StdDraw.filledEllipse(x1 , y1, width, height);
+				}
+				if(filled == false) {
+					StdDraw.ellipse(x1, y1, width, height);
+				}
+				if(shape.equals("triangle")) {
+
+				}
+			}
+
+			in.close();
+		}
+
+
 	}
 }
